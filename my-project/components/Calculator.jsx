2484e63756713784dfb2b20a35c4346fa759d3bc
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2023-10-23 16:29:29 -0300

    Modifier Calculator

--- my-project/components/Calculator.jsx
+++ my-project/components/Calculator.jsx
@@ -1,52 +1,68 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./Calculator.css";
 
-function Calculator() {
+export default function Calculator() {
+  const inputRef = useRef(null);
   const [num, setNum] = useState("");
   const [prevNum, setPrevNum] = useState("");
-  const [operator, setOperator] = useState("");
+  const [operador, setOperador] = useState("");
 
   const calculator = () => {
     if (prevNum !== "") {
-      switch (operator) {
+      switch (operador) {
         case "+":
-          setNum((parseFloat(prevNum) + parseFloat(num)).toString());
-          break;
-        case "-":
-          setNum((parseFloat(prevNum) - parseFloat(num)).toString());
+          setNum((parseFloat(num) + parseFloat(prevNum)).toString());
           break;
         case "x":
-          setNum((parseFloat(prevNum) * parseFloat(num)).toString());
+          setNum((parseFloat(num) * parseFloat(prevNum)).toString());
+          break;
+        case "-":
+          setNum((parseFloat(num) - parseFloat(prevNum)).toString());
           break;
         case "/":
-          setNum((parseFloat(prevNum) / parseFloat(num)).toString());
+          setNum((parseFloat(num) / parseFloat(prevNum)).toString());
           break;
-        default:
-          setNum(num);
+        case "%":
+          setNum(((parseFloat(num) / 100) * parseFloat(prevNum)).toString());
+          break;
       }
-      setPrevNum("");
-      setOperator("");
     }
   };
 
+  const focusInput = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  useEffect(() => {
+    focusInput();
+  }, []);
+
   const HandleButtonClick = (value) => {
-    if (value === "+" || value === "-" || value === "x" || value === "/") {
+    if (
+      value === "+" ||
+      value === "-" ||
+      value === "x" ||
+      value === "/" ||
+      value === "%"
+    ) {
       if (prevNum === "") {
         setPrevNum(num);
       } else {
-        calculator();
         setPrevNum(num);
       }
+      setOperador(value);
       setNum("");
-      setOperator(value);
-    } else if (value === "=") {
-      calculator();
+    } else if (value === "+/-") {
+      const invertedNum = parseFloat(num) * -1;
+      setNum(invertedNum.toString());
     } else if (value === "C") {
       setNum("");
+      setOperador("");
       setPrevNum("");
-      setOperator("");
-    } else if (value === "+/-") {
-      setNum((parseFloat(num) * -1).toString());
+    } else if (value === "=") {
+      calculator();
     } else {
       setNum(num + value);
     }
@@ -54,48 +70,28 @@ function Calculator() {
 
   return (
     <div className="container">
-      <input type="text" value={num} readOnly />
+      <input type="text" value={num} ref={inputRef} />
       <div className="container__button">
-        <button onClick={() => HandleButtonClick("C")} className="orange">
-          C
-        </button>
-        <button onClick={() => HandleButtonClick("+/-")} className="orange">
-          +/-
-        </button>
-        <button onClick={() => HandleButtonClick("%")} className="orange">
-          %
-        </button>
-        <button onClick={() => HandleButtonClick("/")} className="orange">
-          /
-        </button>
+        <button onClick={() => HandleButtonClick("C")}>C</button>
+        <button onClick={() => HandleButtonClick("+/-")}>+/-</button>
+        <button onClick={() => HandleButtonClick("%")}>%</button>
+        <button onClick={() => HandleButtonClick("/")}>/</button>
         <button onClick={() => HandleButtonClick("7")}>7</button>
         <button onClick={() => HandleButtonClick("8")}>8</button>
         <button onClick={() => HandleButtonClick("9")}>9</button>
-        <button onClick={() => HandleButtonClick("x")} className="orange">
-          x
-        </button>
+        <button onClick={() => HandleButtonClick("x")}>x</button>
         <button onClick={() => HandleButtonClick("4")}>4</button>
         <button onClick={() => HandleButtonClick("5")}>5</button>
         <button onClick={() => HandleButtonClick("6")}>6</button>
-        <button onClick={() => HandleButtonClick("-")} className="orange">
-          -
-        </button>
+        <button onClick={() => HandleButtonClick("-")}>-</button>
         <button onClick={() => HandleButtonClick("1")}>1</button>
         <button onClick={() => HandleButtonClick("2")}>2</button>
         <button onClick={() => HandleButtonClick("3")}>3</button>
-        <button onClick={() => HandleButtonClick("+")} className="orange">
-          +
-        </button>
+        <button onClick={() => HandleButtonClick("+")}>+</button>
         <button onClick={() => HandleButtonClick("0")}>0</button>
-        <button onClick={() => HandleButtonClick(".")} className="orange">
-          .
-        </button>
-        <button onClick={() => HandleButtonClick("=")} className="orange">
-          =
-        </button>
+        <button onClick={() => HandleButtonClick(".")}>.</button>
+        <button onClick={() => HandleButtonClick("=")}>=</button>
       </div>
     </div>
   );
 }
-
-export default Calculator;
